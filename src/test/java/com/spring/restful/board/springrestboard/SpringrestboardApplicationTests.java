0b0437681a3fd35b999f40559b8d67fbc66a9c27
iyeongjun <they6687@naver.com>
2019-04-10 14:36:55 +0900

    Initial commit

---
+++ src/test/java/com/spring/restful/board/springrestboard/SpringrestboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.restful.board.springrestboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringrestboardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
